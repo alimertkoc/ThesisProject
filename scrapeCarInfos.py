88c6e9db44483cd701473aad80164fe32017d691
Ali Mert Koç <47777587+alimertkoc@users.noreply.github.com>
2022-04-02 18:44:32 +0300

    2 April Update - scrapeCarInfos Done.

--- scrapeCarInfos.py
+++ scrapeCarInfos.py
@@ -1,21 +1,62 @@
 from bs4 import BeautifulSoup
+import re
 from urllib.request import Request, urlopen
 import requests
 from allConsts import header, header1
 from scrapingbee import ScrapingBeeClient
-#W9LUFY03GBFL3G4IYH1C5NF9PC8LBBG1QJECP350H7EZPKNU208915VPIVQ4HMBAMB3AMCNMQ3RG0IK7
-def getCarDetails(URL):
-    client = ScrapingBeeClient(
-        api_key='W9LUFY03GBFL3G4IYH1C5NF9PC8LBBG1QJECP350H7EZPKNU208915VPIVQ4HMBAMB3AMCNMQ3RG0IK7')
-    response = client.get(URL, headers=header)
+#AEM0KAHPRSGVJK68YKMP6I8THC8LWFKWKI1EWSPUJXDJO7S2R2KZMHM5N43N6S0Z8D6I2JIPIBAMUOOU
+def getCarDetails(client, MAIN_URL):
+    response = client.get(
+        MAIN_URL,
+        params={"premium_proxy": True,
+                "country_code": "tr",
+                "render_js": False
+                }
+    )
 
-    send_request()
-    # page = requests.get(URL, headers=header1)
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    # print(soup)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    #CAR ADVERT NAME
+    car_advert_name = soup.find('h1').get_text()
+
+    #CAR INFO LIST
+    car_brand_name = soup.find_all('ul', class_='classifiedInfoList')[0].get_text().strip()
+    actualCarBrandNames = []
+    car_brand_name_list = car_brand_name.split("\n")
+    for item in car_brand_name_list:
+        item.split()
+        item = item.replace("\xa0","").replace("\t","")
+        if item != '':
+            actualCarBrandNames.append(item)
+
+    car_brand_name = actualCarBrandNames[6].strip()
+    car_series_name = actualCarBrandNames[8].strip()
+    car_model_name = actualCarBrandNames[10].strip()
+    car_year = actualCarBrandNames[13].strip()
+    car_fuel = actualCarBrandNames[16].strip()
+    car_transmission = actualCarBrandNames[19].strip()
+    car_mileage = actualCarBrandNames[22].strip()
+    car_hp = actualCarBrandNames[28].strip()
+    car_engine_size = actualCarBrandNames[31].strip()
+
+    #print(car_brand_name, car_series_name, car_model_name, car_year, car_fuel, car_transmission, car_mileage, car_hp, car_engine_size)
+
+    car_price = soup.findAll('h3')[3].get_text().strip()
+    actualCarList = []
+    car_price_list = car_price.split("\n")
+    for item in car_price_list:
+        if item != "\n":
+            actualCarList.append(item)
+    car_price = actualCarList[0].replace(".","").replace(" TL","")
 
 
-getCarDetails("https://www.sahibinden.com/ilan/vasita-otomobil-bmw-2015-model-bmw-520i-premium-hayalet-m-direksiyon-harmon-cardon-1001212247/detay")
+    #car_brand_name, car_series_name, car_model_name, car_year, car_fuel, car_transmission, car_mileage, car_hp, car_engine_size, car_price
+
+client = ScrapingBeeClient(
+    api_key='GFOIIZMBUYHIG7EVJ0FULYH5XILPIWC37O1IQ7YC2QK8ORVJKX1IVT71DYZFOEEIEKYXO0B1JNTI8DVG')
+
+getCarDetails(client, "https://www.sahibinden.com/ilan/vasita-otomobil-bmw-2016-executive-msport-hatasiz-carbon-schwarz-hayalt-elktrkli-bgj-1010689689/detay")
+
 
 
 
